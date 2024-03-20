9417867eef461b4af79420123006f7b34eadbb25
Mohammad Alamin <alamincsme@gmail.com>
2024-03-20 17:23:51 +0600

    Product

--- src/main/java/io/alamincsme/model/Product.java
+++ src/main/java/io/alamincsme/model/Product.java
@@ -1,9 +1,6 @@
 package io.alamincsme.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +12,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
     private String name;
     private int quantity;
