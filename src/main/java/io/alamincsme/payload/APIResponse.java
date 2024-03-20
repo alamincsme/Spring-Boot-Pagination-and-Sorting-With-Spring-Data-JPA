5e48ba2a4c27cac1926fb3e1ceebdc00b5e9b633
Mohammad Alamin <alamincsme@gmail.com>
2024-03-21 04:46:28 +0600

    API Response

--- src/main/java/io/alamincsme/payload/APIResponse.java
+++ src/main/java/io/alamincsme/payload/APIResponse.java
@@ -1,6 +1,13 @@
 package io.alamincsme.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class APIResponse<T> {
-    int recordCount;
+    Integer recordCount;
     T response;
 }
