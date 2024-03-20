786b12aedc747a7f5e4547bda629f592dc26e1e0
Mohammad Alamin <alamincsme@gmail.com>
2024-03-21 04:47:36 +0600

    Product Service Imp

--- src/main/java/io/alamincsme/service/ProductServiceImpl.java
+++ src/main/java/io/alamincsme/service/ProductServiceImpl.java
@@ -5,6 +5,8 @@ import io.alamincsme.repository.ProductRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,28 +22,30 @@ public class ProductServiceImpl implements ProductService{
 
 //    @PostConstruct
 //    public void initDB() {
-//        List<Product> products = IntStream.rangeClosed(1, 200)
+//        List<Product> products = IntStream.rangeClosed(1, 500)
 //                .mapToObj(i -> new Product("product" + i, new Random().nextInt(100), new Random().nextInt(50000)))
 //                .collect(Collectors.toList());
 //        repository.saveAll(products);
 //    }
     @Override
     public List<Product> findAllProducts() {
-        return null;
+        return repository.findAll() ;
     }
 
-//    @Override
-//    public List<Product> findProductsWithSorting(String field) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Page<Product> findProductsWithPagination(int offset, int pageSize) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Page<Product> findProductsWithPaginationAndSorting(int offset, int pageSize, String field) {
-//        return null;
-//    }
+    @Override
+    public List<Product> findProductsWithSorting(String field) {
+        return repository.findAll(Sort.by(Sort.Direction.ASC, field));
+    }
+
+    @Override
+    public Page<Product> findProductsWithPagination(int offset, int pageSize) {
+        var products =  repository.findAll(PageRequest.of(offset, pageSize));
+        return products ;
+    }
+
+    @Override
+    public Page<Product> findProductsWithPaginationAndSorting(int offset, int pageSize, String field) {
+        var products = repository.findAll(PageRequest.of(offset, pageSize).withSort(Sort.by(Sort.Direction.ASC, field)));
+        return products ;
+    }
 }
