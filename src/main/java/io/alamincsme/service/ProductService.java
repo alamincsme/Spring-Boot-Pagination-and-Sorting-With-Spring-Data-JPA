0974aa663cdba68610d026f5a1561aee84acee65
Mohammad Alamin <alamincsme@gmail.com>
2024-03-20 17:22:28 +0600

    Product Service

--- src/main/java/io/alamincsme/service/ProductService.java
+++ src/main/java/io/alamincsme/service/ProductService.java
@@ -1,4 +1,13 @@
 package io.alamincsme.service;
 
+import io.alamincsme.model.Product;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
 public interface ProductService {
+    public List<Product> findAllProducts();
+//    public List<Product> findProductsWithSorting(String field);
+//    public Page<Product> findProductsWithPagination(int offset, int pageSize);
+//    public Page<Product> findProductsWithPaginationAndSorting(int offset,int pageSize,String field);
 }
