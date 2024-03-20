ff19eb0d30653cfba266127278b7d7a615458a60
Mohammad Alamin <alamincsme@gmail.com>
2024-03-20 16:51:42 +0600

    Spring Boot || Pagination and Sorting

---
+++ src/main/java/io/alamincsme/PaginationAndSortingWithSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package io.alamincsme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationAndSortingWithSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationAndSortingWithSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/alamincsme/PaginationAndSortingWithSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alamincsme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationAndSortingWithSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
